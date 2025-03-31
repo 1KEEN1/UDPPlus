3d2006315570d83b8d5f83ca9c8db58d0227bc3e
Ruslan Pirakov <rpirakov20@gmail.com>
2025-03-31 15:57:40 +0700

    Update header file and add flags for packet.

--- header.h
+++ header.h
@@ -1,28 +1,25 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <arpa/inet.h>
-#include <errno.h>
-#include <linux/types.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <stdint.h>
 
-#define MAX_PACKET_SIZE 1400
 #define SERVER_PORT 8080
+#define MAX_PACKET_SIZE 1400
+
+// Flags
+#define FLAG_SYN 0x01  // Begin connection
+#define FLAG_ACK 0x02  // Acknowledgment
+#define FLAG_FIN 0x04  // Finish
+#define FLAG_DATA 0x08 // Data
 
 typedef struct {
-  __be16 source;  // Port of the source
-  __be16 dest;    // Port of the destination
-  __be32 seq;     // Sequence number
-  __be32 ack_seq; // Acknowledgement number of sequence
+  uint32_t seq;
+  uint32_t ack;
+  uint16_t checksum;
   uint8_t flags;
-  __be16 checksum;
-  __be16 data_length;
-  uint8_t fragment_number;
+  uint16_t data_len;
   uint8_t data[MAX_PACKET_SIZE];
-} Header;
+} MyTransportHeader;
 
 #endif // HEADER_H
