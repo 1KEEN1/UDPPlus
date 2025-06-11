7cede057fd111854e81f41d1d826f8f574c37d5a
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-11 15:39:15 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,111 +1,56 @@
 #include "func.h"
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
-int main() {
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) {
-    perror("Socket error");
-    return 1;
-  }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <encryption_type>\n", argv[0]);
+        printf("Encryption types:\n");
+        printf("0: No encryption\n");
+        printf("1: AES-128-CBC\n");
+        printf("2: XOR\n");
+        return 1;
+    }
 
-  struct sockaddr_in serv_addr = {0};
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(SERVER_PORT);
-  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int enc_type = atoi(argv[1]);
+    if (enc_type < 0 || enc_type > 2) {
+        printf("Invalid encryption type\n");
+        return 1;
+    }
 
-  // Connection
-  MyTransportHeader syn_pkt = {0};
-  syn_pkt.flags = FLAG_SYN;
-  syn_pkt.seq = htonl(12345);
-  syn_pkt.checksum = calculate_checksum(&syn_pkt);
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket error");
+        return 1;
+    }
 
-  if (sendto(sockfd, &syn_pkt, sizeof(syn_pkt), 0,
-             (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("Failed to send SYN");
+    struct sockaddr_in serv_addr = {0};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+    serv_addr.sin_addr.saddr = inet_addr("127.0.0.1");
+
+    if (establish_connection(sockfd, &serv_addr, enc_type) < 0) {
+        close(sockfd);
+        return 1;
+    }
+
+    const char *message = "Hello, custom protocol!";
+    clock_t start_time = clock();
+
+    if (send_reliable_data(sockfd, &serv_addr, message, strlen(message), enc_type, NULL) < 0) {
+        close(sockfd);
+        return 1;
+    }
+
+    clock_t end_time = clock();
+    double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("Data transfer time: %f seconds\n", time_spent);
+
+    close_connection(sockfd, &serv_addr);
     close(sockfd);
-    return 1;
-  }
-
-  // Wait SYN-ACK
-  MyTransportHeader syn_ack;
-  socklen_t addr_len = sizeof(serv_addr);
-  if (recvfrom(sockfd, &syn_ack, sizeof(syn_ack), 0,
-               (struct sockaddr *)&serv_addr, &addr_len) <= 0) {
-    perror("Failed to receive SYN-ACK");
-    close(sockfd);
-    return 1;
-  }
-
-  // Check for checksum
-  uint16_t received_checksum = syn_ack.checksum;
-  syn_ack.checksum = 0;
-  if (received_checksum != calculate_checksum(&syn_ack)) {
-    printf("SYN-ACK checksum mismatch!\n");
-    close(sockfd);
-    return 1;
-  }
-  syn_ack.checksum = received_checksum;
-
-  // Send ACK
-  MyTransportHeader ack_pkt = {0};
-  ack_pkt.flags = FLAG_ACK;
-  ack_pkt.ack = syn_ack.seq + 1;
-  ack_pkt.checksum = calculate_checksum(&ack_pkt);
-
-  if (sendto(sockfd, &ack_pkt, sizeof(ack_pkt), 0,
-             (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("Failed to send ACK");
-    close(sockfd);
-    return 1;
-  }
-
-  // Send data
-  const char *message = "Hello, custom protocol!";
-  MyTransportHeader data_pkt = {0};
-  data_pkt.flags = FLAG_DATA;
-  data_pkt.seq = htonl(12346);
-  data_pkt.data_len = htons(strlen(message));
-  memcpy(data_pkt.data, message, strlen(message));
-  data_pkt.checksum = calculate_checksum(&data_pkt);
-
-  if (sendto(sockfd, &data_pkt, sizeof(data_pkt), 0,
-             (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("Failed to send data");
-    close(sockfd);
-    return 1;
-  }
-
-  // Wait for ACK
-  MyTransportHeader data_ack;
-  if (recvfrom(sockfd, &data_ack, sizeof(data_ack), 0,
-               (struct sockaddr *)&serv_addr, &addr_len) <= 0) {
-    perror("Failed to receive DATA ACK");
-    close(sockfd);
-    return 1;
-  }
-
-  // Check for checksum
-  received_checksum = data_ack.checksum;
-  data_ack.checksum = 0;
-  if (received_checksum != calculate_checksum(&data_ack)) {
-    printf("DATA ACK checksum mismatch!\n");
-    close(sockfd);
-    return 1;
-  }
-
-  printf("Data sent and acknowledged!\n");
-
-  // Close the connection
-  MyTransportHeader fin_pkt = {0};
-  fin_pkt.flags = FLAG_FIN;
-  fin_pkt.checksum = calculate_checksum(&fin_pkt);
-
-  sendto(sockfd, &fin_pkt, sizeof(fin_pkt), 0, (struct sockaddr *)&serv_addr,
-         sizeof(serv_addr));
-
-  close(sockfd);
-  return 0;
+    return 0;
 }
