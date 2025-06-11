639af8bbcff331af53779fa326ba3ecef7ff76eb
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-11 15:37:47 +0700

    Update header.h

--- header.h
+++ header.h
@@ -4,20 +4,21 @@
 #include <netinet/in.h>
 #include <openssl/evp.h>
 #include <stdint.h>
+#include <time.h>
 
 #define SERVER_PORT 8080
 #define MAX_PACKET_SIZE 1400
 
-// Encryption type
+// Encryption types
 #define ENCRYPTION_NONE 0
 #define ENCRYPTION_AES 1
 #define ENCRYPTION_XOR 2
 
-// AES size
+// AES settings
 #define AES_KEY_SIZE 16
 #define AES_IV_SIZE 16
 
-// XOR key (пример)
+// XOR key
 #define XOR_KEY_SIZE 4
 static const uint8_t xor_key[XOR_KEY_SIZE] = {0xAA, 0xBB, 0xCC, 0xDD};
 
@@ -28,21 +29,21 @@ static const uint8_t xor_key[XOR_KEY_SIZE] = {0xAA, 0xBB, 0xCC, 0xDD};
 #define FLAG_DATA 0x08
 
 typedef struct {
-  uint32_t seq;
-  uint32_t ack;
-  uint16_t checksum;
-  uint8_t flags;
-  uint8_t enc_type; // Encryption type
-  uint16_t data_len;
-  union {
-    struct {
-      uint8_t iv[AES_IV_SIZE]; // AES
-      uint8_t data[MAX_PACKET_SIZE];
-    } aes;
-    struct {
-      uint8_t data[MAX_PACKET_SIZE]; // XOR / без шифрования
-    } no_enc;
-  };
+    uint32_t seq;
+    uint32_t ack;
+    uint16_t checksum;
+    uint8_t flags;
+    uint8_t enc_type; // Encryption type
+    uint16_t data_len;
+    union {
+        struct {
+            uint8_t iv[AES_IV_SIZE]; // AES IV
+            uint8_t data[MAX_PACKET_SIZE];
+        } aes;
+        struct {
+            uint8_t data[MAX_PACKET_SIZE]; // XOR / No encryption
+        } no_enc;
+    };
 } MyTransportHeader;
 
 #endif // HEADER_H
