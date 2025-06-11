a11b99488c11d51b90bba81caf600aed0d3a2a06
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-11 15:45:15 +0700

    Update func.h

--- func.h
+++ func.h
@@ -9,6 +9,7 @@ void crypto_init();
 // Encryption/Decryption functions
 int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
+void xor_encrypt_decrypt(MyTransportHeader *pkt);
 
 uint16_t calculate_checksum(const MyTransportHeader *header);
 int establish_connection(int sockfd, struct sockaddr_in *addr, int enc_type);
