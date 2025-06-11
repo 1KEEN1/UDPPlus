837e89a639d7a2e9c13792fe2fa91bac2efb4838
Ruslan Pirakov <rpirakov20@gmail.com>
2025-06-11 12:06:05 +0700

    Update.

--- func.h
+++ func.h
@@ -3,14 +3,16 @@
 
 #include "header.h"
 
-// 3-way handshake
-int establish_connection(int sockfd, struct sockaddr_in *addr);
+// Initialize crypto
+void crypto_init();
 
-int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data,
-                       size_t len);
-
-void close_connection(int sockfd, struct sockaddr_in *addr);
+// Encryption/Decryption functions
+int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
+int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 
 uint16_t calculate_checksum(const MyTransportHeader *header);
+int establish_connection(int sockfd, struct sockaddr_in *addr);
+int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data, size_t len);
+void close_connection(int sockfd, struct sockaddr_in *addr);
 
 #endif // FUNC_H
