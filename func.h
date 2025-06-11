90f266c389dc12d312d7f78c59138f78ef88263b
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-11 15:43:42 +0700

    Update func.h

--- func.h
+++ func.h
@@ -11,8 +11,8 @@ int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 
 uint16_t calculate_checksum(const MyTransportHeader *header);
-int establish_connection(int sockfd, struct sockaddr_in *addr);
-int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data, size_t len);
+int establish_connection(int sockfd, struct sockaddr_in *addr, int enc_type);
+int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data, size_t len, int enc_type, const uint8_t *aes_key);
 void close_connection(int sockfd, struct sockaddr_in *addr);
 
 #endif // FUNC_H
