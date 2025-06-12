624003ce5207debec73685737d310d6b28c89a39
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-12 11:58:35 +0700

    Update func.h

--- func.h
+++ func.h
@@ -3,21 +3,12 @@
 
 #include "header.h"
 
-#define WINDOW_SIZE 32
-#define CHUNK_SIZE (64 * 1024)
-#define SOCKET_BUF_SIZE (1024 * 1024)
-
-// Initialize crypto
+// Crypto functions
 void crypto_init();
-
-// Encryption/Decryption functions
 int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
-void xor_encrypt_decrypt(MyTransportHeader *pkt);
 
+// Protocol functions
 uint16_t calculate_checksum(const MyTransportHeader *header);
-int establish_connection(int sockfd, struct sockaddr_in *addr, int enc_type);
-int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data, size_t len, int enc_type, const uint8_t *aes_key);
-void close_connection(int sockfd, struct sockaddr_in *addr);
 
 #endif // FUNC_H
