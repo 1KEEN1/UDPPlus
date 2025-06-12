41445c9329f019ebe129bda3b88515fd286732cd
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-12 12:21:22 +0700

    Update func.h

--- func.h
+++ func.h
@@ -7,6 +7,7 @@
 void crypto_init();
 int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
+void xor_encrypt_decrypt(MyTransportHeader *pkt); // Добавлена декларация
 
 // Protocol functions
 uint16_t calculate_checksum(const MyTransportHeader *header);
