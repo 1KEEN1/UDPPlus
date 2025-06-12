530293984791b750949816aafc33330197d9e40a
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-12 12:28:39 +0700

    Update header.h

--- header.h
+++ header.h
@@ -2,7 +2,9 @@
 #define HEADER_H
 
 #include <netinet/in.h>
+#include <openssl/evp.h>
 #include <stdint.h>
+#include <time.h>
 
 #define SERVER_PORT 8080
 #define MAX_PACKET_SIZE 1400
@@ -12,7 +14,7 @@
 #define ENCRYPTION_AES 1
 #define ENCRYPTION_XOR 2
 
-// AES constants
+// AES settings
 #define AES_KEY_SIZE 16
 #define AES_IV_SIZE 16
 
@@ -20,7 +22,7 @@
 #define XOR_KEY_SIZE 4
 static const uint8_t xor_key[XOR_KEY_SIZE] = {0xAA, 0xBB, 0xCC, 0xDD};
 
-// Packet flags
+// Flags
 #define FLAG_SYN 0x01
 #define FLAG_ACK 0x02
 #define FLAG_FIN 0x04
@@ -31,15 +33,15 @@ typedef struct {
     uint32_t ack;
     uint16_t checksum;
     uint8_t flags;
-    uint8_t enc_type;
+    uint8_t enc_type; // Encryption type
     uint16_t data_len;
     union {
         struct {
-            uint8_t iv[AES_IV_SIZE];
+            uint8_t iv[AES_IV_SIZE]; // AES IV
             uint8_t data[MAX_PACKET_SIZE];
         } aes;
         struct {
-            uint8_t data[MAX_PACKET_SIZE];
+            uint8_t data[MAX_PACKET_SIZE]; // XOR / No encryption
         } no_enc;
     };
 } MyTransportHeader;
