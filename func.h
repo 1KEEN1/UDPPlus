3d4090f69f1d2870b179fbd95475d69f0a3fe3eb
Pirakov Ruslan <rpirakov20@gmail.com>
2025-06-12 12:28:51 +0700

    Update func.h

--- func.h
+++ func.h
@@ -3,13 +3,17 @@
 
 #include "header.h"
 
-// Crypto functions
+// Initialize crypto
 void crypto_init();
+
+// Encryption/Decryption functions
 int encrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
 int decrypt_packet(MyTransportHeader *pkt, const uint8_t *aes_key);
-void xor_encrypt_decrypt(MyTransportHeader *pkt); // Добавлена декларация
+void xor_encrypt_decrypt(MyTransportHeader *pkt);
 
-// Protocol functions
 uint16_t calculate_checksum(const MyTransportHeader *header);
+int establish_connection(int sockfd, struct sockaddr_in *addr, int enc_type);
+int send_reliable_data(int sockfd, struct sockaddr_in *addr, const void *data, size_t len, int enc_type, const uint8_t *aes_key);
+void close_connection(int sockfd, struct sockaddr_in *addr);
 
 #endif // FUNC_H
